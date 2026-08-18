4eaef65b8d4fed51d6cc76646e2400e5e09176ae
Sriyut Singh <sriyutsingh5@gmail.com>
2026-08-18 23:41:39 +0530

    Refactor removeDuplicates method to remove duplicates

--- Python-Solutions/0026_Remove_Duplicates_from_Sorted_Array.py
+++ Python-Solutions/0026_Remove_Duplicates_from_Sorted_Array.py
@@ -16,7 +16,6 @@ for (int i = 0; i < k; i++) {
     assert nums[i] == expectedNums[i];
 }
 If all assertions pass, then your solution will be accepted.
-
  
 
 Example 1:
@@ -39,11 +38,10 @@ class Solution:
     def removeDuplicates(self, nums):
         if not nums:
             return 0
-
+            
         k = 1
         for i in range(1, len(nums)):
             if nums[i] != nums[k - 1]:
                 nums[k] = nums[i]
                 k += 1
         return k
-
